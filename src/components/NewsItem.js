eef34d86747e6760016d3d444cfbc59b381c3022
Neha <codewithneha2020@gmail.com>
2022-01-18 19:51:42 +0530

    adding time date and source badge

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,14 +4,15 @@ import React, { Component } from 'react'
 export default class NewsItem extends Component {
   
     render() {
-       let {title, description,imageUrl,newsUrl} = this.props;
+       let {title, description,imageUrl,newsUrl,author, date,source} = this.props;
         return (
             <div className="my-3">
               <div className="card">
                     <img src={!imageUrl?"https://c.ndtvimg.com/2022-01/shb79h9g_taliban-musical-instrument_625x300_16_January_22.jpg":imageUrl} className="card-img-top" alt="... "/>
                      <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
+                        <h5 className="card-title">{title} <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source}</span></h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                     </div>
